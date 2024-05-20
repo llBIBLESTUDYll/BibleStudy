77f5f361a824d681d787af408381692d82a2634f
alilee0923 <ali.lee.0923@gmail.com>
2024-05-20 19:42:15 -0400

    typo fix

--- app/config/aws_exports.js
+++ app/config/aws_exports.js
@@ -4,6 +4,6 @@ const awsmobile = {
    aws_cognito_region: 'us-east-2',
    aws_user_pools_id: 'us-east-2_KxbN50OAV',
    aws_user_pools_web_client_id: '7o1nvk96m9hrcm2eqgpefsium6',
-   oauth: {},
+   oauth: {}
 };
 export default awsmobile;
