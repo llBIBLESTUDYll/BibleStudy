19c75f5845bf22e61883dc5dda7de43889f987f7
alilee0923 <ali.lee.0923@gmail.com>
2024-05-21 07:14:25 -0400

    final

--- app/config/aws_exports.js
+++ app/config/aws_exports.js
@@ -1,9 +1,9 @@
-const awsmobile = {
-   aws_project_region: 'us-east-2',
-   aws_cognito_identity_pool_id: 'us-east-2:1a3a6f94-ce7b-4a05-8615-5fd277c3a733',
-   aws_cognito_region: 'us-east-2',
-   aws_user_pools_id: 'us-east-2_KxbN50OAV',
-   aws_user_pools_web_client_id: '7o1nvk96m9hrcm2eqgpefsium6',
-   oauth: {}
-};
-export default awsmobile;
+ const awsmobile = {
+    aws_project_region: 'us-east-2',
+    aws_cognito_identity_pool_id: 'us-east-2:1a3a6f94-ce7b-4a05-8615-5fd277c3a733',
+    aws_cognito_region: 'us-east-2',
+    aws_user_pools_id: 'us-east-2_KxbN50OAV',
+    aws_user_pools_web_client_id: '7o1nvk96m9hrcm2eqgpefsium6',
+    oauth: {},
+ };
+ export default awsmobile;
